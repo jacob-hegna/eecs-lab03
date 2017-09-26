a71c0453ddb64578277ddc5ee6b3cf3db0d680cd
Jacob Hegna <jacobhegna@gmail.com>
2017-09-26 00:30:31 -0500

    Make images local data instead of remote

--- exercise-2/main.js
+++ exercise-2/main.js
@@ -1,9 +1,9 @@
 let images = [
-    'https://i.imgur.com/jIieD5A.png',
-    'https://i.redd.it/kyktro2pgwkz.jpg',
-    'https://i.redd.it/cyw0nduqiaiz.jpg',
-    'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a6/Cubic_with_double_point.svg/1200px-Cubic_with_double_point.svg.png',
-    'https://upload.wikimedia.org/wikipedia/commons/thumb/e/ed/Cokernel-02.png/200px-Cokernel-02.png'
+    'img/1.png',
+    'img/2.jpg',
+    'img/3.jpg',
+    'img/4.png',
+    'img/5.png'
 ];
 
 let image_idx = 0;
